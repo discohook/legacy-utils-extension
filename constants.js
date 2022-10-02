304ef9d6e042dcd3df36a20d9e458b09997bcfbc
shay <43248357+shayypy@users.noreply.github.com>
2022-10-02 15:02:26 -0500

    Adjust snowflake max length

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const DUTILS_API = 'https://dutils.shay.cat/api/v1'
-const SNOWFLAKE_RE = /\d{17,22}/
+const SNOWFLAKE_RE = /\d{16,19}/
 
 console.log('[Discohook Utils] Loaded constants', { DUTILS_API, SNOWFLAKE_RE })
