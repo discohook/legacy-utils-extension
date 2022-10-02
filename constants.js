1fe20b89f680f9141cc5ebf81b901e5619cbc449
shay <43248357+shayypy@users.noreply.github.com>
2022-10-02 16:23:38 -0500

    Move constants log to debug

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 const DUTILS_API = 'https://dutils.shay.cat/api/v1'
 const SNOWFLAKE_RE = /\d{16,19}/
 
-console.log('[Discohook Utils] Loaded constants', { DUTILS_API, SNOWFLAKE_RE })
+console.log('[Discohook Utils] Loaded constants')
+console.debug({ DUTILS_API, SNOWFLAKE_RE })
