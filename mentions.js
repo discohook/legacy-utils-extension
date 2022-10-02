412c9bd3eed0fc725b622f2642a4346804e0d1ed
shay <43248357+shayypy@users.noreply.github.com>
2022-10-02 14:51:13 -0500

    Load role mentions

--- mentions.js
+++ mentions.js
@@ -26,9 +26,26 @@ const getMentions = () => {
   }
 }
 
+// https://stackoverflow.com/a/11866980
+function toRGBA(num, opacity=1) {
+  num >>>= 0
+  const b = num & 0xFF,
+        g = (num & 0xFF00) >>> 8,
+        r = (num & 0xFF0000) >>> 16
+  return 'rgba(' + [r, g, b, opacity].join(',') + ')'
+}
+
+const resetStyle = (element) => {
+  element.style.color = null
+  element.style.backgroundColor = null
+  element.onmouseover = null
+  element.onmouseout = null
+}
+
 const replaceMention = async (mention) => {
   if (invalidEntityCache[mention.type].includes(mention.id)) {
     mention.element.innerText = `${entityCache[mention.type]._prefix}invalid-${mention.type}`
+    resetStyle(mention.element)
     return
   }
   let cached = entityCache[mention.type][mention.id]
@@ -39,9 +56,9 @@ const replaceMention = async (mention) => {
       case 'user':
         route = `/users/${mention.id}`
         break
-      //case 'role':
-      //  route = `/roles/${mention.id}`
-      //  break
+      case 'role':
+        route = `/roles/${mention.id}`
+        break
       //case 'channel':
       //  route = `/channels/${mention.id}`
       //  break
@@ -55,6 +72,7 @@ const replaceMention = async (mention) => {
       if (!response.ok) {
         invalidEntityCache[mention.type].push(mention.id)
         mention.element.innerText = `${entityCache[mention.type]._prefix}invalid-${mention.type}`
+        resetStyle(mention.element)
         console.error(data)
         return
       }
@@ -63,10 +81,17 @@ const replaceMention = async (mention) => {
     } else {
       console.debug(`[Discohook Utils] Skipping ${mention.type} with ID ${mention.id}; unrecognized type or invalid snowflake`)
       mention.element.innerText = `${entityCache[mention.type]._prefix}${mention.type}`
+      resetStyle(mention.element)
       return
     }
   }
   mention.element.innerText = `${entityCache[mention.type]._prefix}${cached.name}`
+  if (cached.color) {
+    mention.element.style.color = toRGBA(cached.color)
+    mention.element.style.backgroundColor = toRGBA(cached.color, 0.1)
+    mention.element.onmouseover = () => {mention.element.style.backgroundColor = toRGBA(cached.color, 0.3)}
+    mention.element.onmouseout = () => {mention.element.style.backgroundColor = toRGBA(cached.color, 0.1)}
+  }
 }
 
 const replaceAllMentions = () => {
@@ -79,5 +104,7 @@ document.addEventListener('input', (e) => {
     replaceAllMentions()
   }
 })
+
+document.addEventListener('load', replaceAllMentions)
 
 console.log('[Discohook Utils] Loaded mentions')
